629b4c6e91f2c1c693ad0ecbca5617c70d6df12f
Oussama-kh <93738555+oussama22kh@users.noreply.github.com>
2023-12-28 12:32:02 +0100

    cleanig

--- DAW/Alert/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Alert
-
-# Register your models here.
-
-admin.site.register(Alert)
-
-
--- DAW/Quiz/urls.py
+++
@@ -1,6 +0,0 @@
-
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('get_quizs/',views.get_quizs,name='get_quizs')
-]
